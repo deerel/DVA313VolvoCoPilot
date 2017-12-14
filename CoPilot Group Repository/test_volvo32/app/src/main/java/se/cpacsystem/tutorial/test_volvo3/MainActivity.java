ea90fd64649562f4bdd59cf668a3579d49d36959
Dara <dai15002@student.mdh.se>
2017-12-14 13:16:28 +0100

    seems to "work"

---
+++ CoPilot Group Repository/test_volvo32/app/src/main/java/se/cpacsystem/tutorial/test_volvo3/MainActivity.java
@@ -0,0 +1,15 @@
+package se.cpacsystem.tutorial.test_volvo3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//import se.cpacsystems.camera.CameraManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
