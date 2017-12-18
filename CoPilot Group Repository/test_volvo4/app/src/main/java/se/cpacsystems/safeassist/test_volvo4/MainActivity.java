e14fa57be66c00ff7be3e8845573e3d473286db3
pablo-dlf <pablo-dlf@users.noreply.github.com>
2017-12-18 18:20:14 +0100

    monday

--- CoPilot Group Repository/test_volvo4/app/src/main/java/se/cpacsystems/safeassist/test_volvo4/MainActivity.java
+++ CoPilot Group Repository/test_volvo4/app/src/main/java/se/cpacsystems/safeassist/test_volvo4/MainActivity.java
@@ -5,11 +5,19 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import se.cpacsystems.common.Position;
-import se.cpacsystems.position.ProjPosition;
-import se.cpacsystems.position.PositionManager;
+//import se.cpacsystems.common.Position;
+//import se.cpacsystems.position.ProjPosition;
+//import se.cpacsystems.position.PositionManager;
+
+import com.volvo.softproduct.sensorextensionlibrary.db_enum.art_data;
+import com.volvo.softproduct.sensorextensionlibrary.db_enum.exc_data;
+import com.volvo.softproduct.sensorextensionlibrary.db_enum.gnss_data;
+import com.volvo.softproduct.sensorextensionlibrary.db_enum.wlo_data;
+import com.volvo.softproduct.sensorextensionlibrary.db_value.float_value;
+import com.volvo.softproduct.sensorextensionlibrary.managers.*;
 public class MainActivity extends AppCompatActivity {
     TextView unit_type;
+    private gnss_manager _gdm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void check_unit(View v){
         try{
-            PositionManager pos_obj = new PositionManager(this);
-            Position unit_pos = pos_obj.getPosition();
-            Toast.makeText(getApplicationContext(), "Your position is: "+unit_pos.toString(), Toast.LENGTH_LONG).show();
+            float_value dvaluelat = _gdm.getFloatSignal(gnss_data.latitude.getCode());
+            float_value dvaluelong = _gdm.getFloatSignal(gnss_data.longitude.getCode());
+            float_value dvaluealt = _gdm.getFloatSignal(gnss_data.altitude.getCode());
+            //_gpsposition.setText(String.format("Lat %.5f Long %.5f Altitude %.5f\n",dvaluelat.getValue(),dvaluelong.getValue(),dvaluealt.getValue()));
+           /* PositionManager pos_obj = new PositionManager(this);
+            Position unit_pos = pos_obj.getPosition();*/
+            Toast.makeText(getApplicationContext(), "latitude is: "+dvaluelat.getValue()+ "AND longitude is: "+dvaluelong.getValue(), Toast.LENGTH_LONG).show();
             unit_type.setText("You are on a CoPilot");
         }catch (Exception e){
             unit_type.setText("You are on a hand-held device");
